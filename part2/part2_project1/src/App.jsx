3bef141847009b32d1a9987ff11bf765aa173867
tukkimiesjonni <tukkimiesjonni@gmail.com>
2025-05-30 01:35:08 +0300

    Added exercise 2.1 modifications

---
+++ part2/part2_project1/src/App.jsx
@@ -0,0 +1,114 @@
+const Course = (props) => {
+  return (
+    <div>
+      <div>
+        <Header course={props.course}/>
+      </div>
+      <div>
+        <Content parts={props.course.parts}/>
+      </div>
+    </div>
+  )
+}
+
+const Header = (props) => <h1>{props.course.name}</h1>
+
+const Content = (props) => {
+  return (
+    <div>
+      {props.parts.map(part => (
+        <Part key={part.id} part={part}/>
+      ))}
+    </div>
+  )
+}
+
+const Part = (props) => (
+  <p>
+    {props.part.name} {props.part.exercises}
+  </p>
+)
+
+const Total = (props) => <p>Number of exercises {props.total}</p>
+
+const App = () => {
+  const course = {
+    name: 'Half Stack application development',
+    id: 1,
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3
+      }
+    ]
+  }
+
+  return (
+    <div>
+      <Course course={course} />
+    </div>
+  )
+}
+
+export default App
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+const App = () => {
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+      },
+    ],
+  }
+
+  return (
+    <div>
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total
+        total={
+          course.parts[0].exercises +
+          course.parts[1].exercises +
+          course.parts[2].exercises
+        }
+      />
+    </div>
+  )
+}
+
+export default App
+
+*/
