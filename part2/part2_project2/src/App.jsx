5e0d18eb1c69b4f6bf8c2e4aefb35a388594c53a
tukkimiesjonni <tukkimiesjonni@gmail.com>
2025-06-11 12:01:03 +0300

    Added exercise 2.12 modifications

--- part2/part2_project2/src/App.jsx
+++ part2/part2_project2/src/App.jsx
@@ -95,9 +95,13 @@ useEffect(hook, [])
         number: newNumber
       }
 
-      setPersons(persons.concat(personObject))
-      setNewName('')
-      setNewNumber('')
+      axios
+      .post('http://localhost:3001/persons', personObject)
+      .then(response => {
+        setPersons(persons.concat(response.data))
+        setNewName('')
+        setNewNumber('')
+      })
     }
   }
 
