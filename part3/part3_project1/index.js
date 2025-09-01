f74057dcd432d89eed6850f592c9a04b68450097
tukkimiesjonni <tukkimiesjonni@gmail.com>
2025-09-01 15:39:35 +0300

    Added exercise 3.3 modifications

--- part3/part3_project1/index.js
+++ part3/part3_project1/index.js
@@ -72,6 +72,20 @@ app.get('/api/persons', (request, response) => {
   response.json(persons)
 })
 
+app.get('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  const person = persons.find(per => per.id === id)
+
+  if (person) {
+    response.json(person)
+  }
+  else {
+    response.status(404).end()
+  }
+
+  response.json(person)
+})
+
 
 const generateId = () => {
   const maxId = notes.length > 0
