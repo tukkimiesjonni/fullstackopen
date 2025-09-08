d10791a63be33381f3b16a1b5a10a2d6d185639c
tukkimiesjonni <tukkimiesjonni@gmail.com>
2025-09-08 15:33:57 +0300

    Added exercise 3.9 modifications

--- part3/part3_project1/index.js
+++ part3/part3_project1/index.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const morgan = require('morgan')
 const app = express()
+const cors = require('cors')
 
 app.use(express.json())
 app.use(morgan('tiny'))
+app.use(cors())
 
 let persons = [
   {
