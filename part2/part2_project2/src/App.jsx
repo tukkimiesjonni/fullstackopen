7f1d9de4d1f5aeca5e065f7a2da91a998b31dcc8
tukkimiesjonni <tukkimiesjonni@gmail.com>
2025-09-22 23:31:13 +0300

    Updated frontend for exercises in part 3

--- part2/part2_project2/src/App.jsx
+++ part2/part2_project2/src/App.jsx
@@ -191,7 +191,7 @@ const App = () => {
       .catch(error => {
         setMessageType("error"),
         setErrorMessage(
-          "Error creating a new contact"
+          error.response.data.error
         ),
         setTimeout(() => {
           setErrorMessage(null)
